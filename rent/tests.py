7372b3d3ac1d375e4f7b503e52f3dc538d7a918b
Víctor Gómez <vgomez@applaudostudios.dev>
2022-03-21 17:12:44 -0400

    Update test code

--- rent/tests.py
+++ rent/tests.py
@@ -2,10 +2,10 @@ from django.core.exceptions import ValidationError
 from datetime import date
 from django.test import TestCase
 
-
 from film.models import Film, Category
 from person.models import Person, Client
 from rent.business_logic import RentBusinessLogic
+from rent.validations import RentValidation
 from rent.models import Rent
 
 
@@ -60,37 +60,37 @@ class RentAppTestCase(TestCase):
         with self.assertRaises(ValidationError):
             date1 = date(year=2022, month=1, day=17)
             date2 = date(year=2022, month=1, day=1)
-            RentBusinessLogic.validate_date_gt_max_limit(date1, date2, "test")
+            RentValidation.validate_date_gt_max_limit(date1, date2, "test")
 
     def test_validate_date1_gr_or_eq_date2(self):
         with self.assertRaises(ValidationError):
             date1 = date(year=2022, month=1, day=1)
             date2 = date(year=2022, month=1, day=1)
-            RentBusinessLogic.validate_date1_gr_or_eq_date2(date1, date2,
-                                                            "test")
+            RentValidation.validate_date1_gr_or_eq_date2(date1, date2,
+                                                         "test")
 
         with self.assertRaises(ValidationError):
             date1 = date(year=2022, month=1, day=1)
             date2 = date(year=2022, month=1, day=2)
-            RentBusinessLogic.validate_date1_gr_or_eq_date2(date1, date2,
-                                                            "test")
+            RentValidation.validate_date1_gr_or_eq_date2(date1, date2,
+                                                         "test")
 
     def test_validate_date1_eq_or_low_date2(self):
         with self.assertRaises(ValidationError):
             date1 = date(year=2022, month=1, day=1)
             date2 = date(year=2022, month=1, day=1)
-            RentBusinessLogic.validate_date1_eq_or_low_date2(date1, date2,
-                                                             "test")
+            RentValidation.validate_date1_eq_or_low_date2(date1, date2,
+                                                          "test")
 
             date1 = date(year=2022, month=1, day=1)
             date2 = date(year=2022, month=1, day=2)
-            RentBusinessLogic.validate_date1_eq_or_low_date2(date1, date2,
-                                                             "test")
+            RentValidation.validate_date1_eq_or_low_date2(date1, date2,
+                                                          "test")
 
     def test_validate_amount_update(self):
         with self.assertRaises(ValidationError):
-            RentBusinessLogic.validate_amount_update(self.amount, self.film2,
-                                                     self.rent1)
+            RentValidation.validate_amount_update(self.amount, self.film2,
+                                                  self.rent1)
 
     def test_get_rent_cost(self):
         cost = 20
@@ -113,7 +113,7 @@ class RentAppTestCase(TestCase):
 
     def test_validate_state_close(self):
         with self.assertRaises(ValidationError):
-            RentBusinessLogic.validate_state_close('close', None)
+            RentValidation.validate_state_close('close', None)
 
     def test_state_close_return_films(self):
         amount = 2
@@ -124,4 +124,4 @@ class RentAppTestCase(TestCase):
 
     def test_validate_update_permission(self):
         with self.assertRaises(ValidationError):
-            RentBusinessLogic.validate_update_permission('close')
+            RentValidation.validate_update_permission('close')
